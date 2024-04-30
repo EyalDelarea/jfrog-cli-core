55dfe4447238aa25ab05f93151a6ba260b102865
delarea <eyalde@jfrog.com>
2024-04-30 17:27:32 +0300

    generate markdown with link

--- artifactory/utils/filetree_test.go
+++ artifactory/utils/filetree_test.go
@@ -24,3 +24,22 @@ func TestFileTree(t *testing.T) {
 	result, excpected = fileTree.String(), ""
 	assert.Equal(t, excpected, result)
 }
+
+func TestFileTreeWithLinks(t *testing.T) {
+	copyMaxFilesInTree := maxFilesInTree
+	defer func() {
+		maxFilesInTree = copyMaxFilesInTree
+	}()
+	maxFilesInTree = 1
+
+	fileTree := NewFileTree()
+	// Add a new file and check String()
+	fileTree.AddFile("repoName/path/to/first/artifact")
+	result, excpected := fileTree.StringWithLinks("http://test.com"), "📦 repoName\n└── 📁 path\n    └── 📁 to\n        └── 📁 first\n            └── 📄 [artifact](http://test.com)\n"
+	assert.Equal(t, excpected, result)
+
+	// If maxFileInTree has exceeded, Check String() returns an empty string
+	fileTree.AddFile("repoName/path/to/second/artifact")
+	result, excpected = fileTree.String(), ""
+	assert.Equal(t, excpected, result)
+}
